6dc1547d0856e0ffa981897225aabc6228afdcbf
Franco Schmidt <francosr98@gmail.com>
2023-04-20 14:13:38 -0300

    Minimal working packer install with first-run flag

---
+++ lua/core/plugins.lua
@@ -0,0 +1,59 @@
+	-- Function to install packer from github
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		print("Packer was not found.")
+		print("Currently installing to " .. install_path .. ".")
+		print("\n")
+		fn.system({"git", "clone", "--depth", "1",
+				"https://github.com/wbthomason/packer.nvim", install_path})
+		vim.cmd [[packadd packer.nvim]]
+		return true
+	end
+	return false
+end
+
+packer_bootstrap = ensure_packer()
+
+if packer_bootstrap then
+	print("Instalation done. Now installing plugins.")
+	print("\n")
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+	augroup packer_user_config
+		autocmd!
+		autocmd BufWritePost plugins.lua source <afile> | PackerSync
+	augroup end
+]])
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not packer_bootstrap and not status_ok then
+	print("IMPORTANT - SOMETHING WENT WRONG WITH PACKER:", packer)
+	return
+end
+
+-- Have packer use a popup window
+packer.init({
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
+})
+
+return packer.startup(function(use)
+	use { "wbthomason/packer.nvim" } -- Have packer manage itself
+
+	use { "nvim-lua/plenary.nvim" }
+
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if packer_bootstrap then
+		packer.sync()
+	end
+end)
+
