a3bf772de08b4819ea9e2f0365f520101cb1e1f2
Franco Schmidt <francosr98@gmail.com>
2023-04-30 11:31:56 -0300

    Forgot to add to second to last commit

--- lua/conf/illuminate.lua
+++ lua/conf/illuminate.lua
@@ -1,5 +1,10 @@
 local cmp_status_ok, illuminate = pcall(require, "illuminate")
 if not cmp_status_ok then
-	vim.notify("Something went wrong with illuminate: " .. illuminate)
+	vim.notify("Something went wrong with illuminate", vim.log.levels.WARN)
+	-- vim.notify("" .. illuminate, vim.log.levels.DEBUG)
 	return
 end
+
+illuminate.configure({
+	delay = 0,
+})
