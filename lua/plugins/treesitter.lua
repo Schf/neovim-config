f45d30111479ac5f858c9931e704996e080f9ad7
Franco Schmidt <francosr98@gmail.com>
2023-05-04 16:25:56 -0300

    Adding Treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,73 @@
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = function()
+			require("nvim-treesitter.install").update({ with_sync = true })
+		end,
+		config = function()
+			configs = require("nvim-treesitter.configs")
+			configs.setup({
+				ensure_installed = {
+					"lua",
+
+					-- "all",
+
+					-- "bash",
+					-- "c",
+					-- "cmake",
+					-- "cpp",
+					-- "cuda",
+					-- "git_config",
+					-- "git_rebase",
+					-- "gitattributes",
+					-- "gitcommit",
+					-- "gitignore",
+					-- "java",
+					-- "json",
+					-- "llvm",
+					-- "lua",
+					-- "markdown",
+					-- "markdown_inline",
+					-- "python",
+					-- "rust",
+					-- "toml",
+					-- "yaml",
+				},
+				-- one of "all" or a list of languages
+				ignore_install = { "phpdoc" },
+				highlight = {
+					enable = true,    -- false will disable the whole extension
+				},
+				autopairs = {
+					enable = true,
+				},
+				indent = {
+					enable = true,
+				},
+				-- context_commentstring = {
+				-- 	enable = true,
+				-- 	enable_autocmd = false,
+				-- },
+				playground = {
+					enable = true,
+					disable = {},
+					updatetime = 25,
+					persist_queries = false,
+					keybindings = {
+						toggle_query_editor = 'o',
+						toggle_hl_groups = 'i',
+						toggle_injected_languages = 't',
+						toggle_anonymous_nodes = 'a',
+						toggle_language_display = 'I',
+						focus_language = 'f',
+						unfocus_language = 'F',
+						update = 'R',
+						goto_node = '<cr>',
+						show_help = '?',
+					},
+				},
+			})
+		end,
+	},
+	{ "nvim-treesitter/playground",	},
+}
