b1d4a3b7a63fe8b9fe800c0e37e4080ef24740b8
Franco Schmidt <francosr98@gmail.com>
2023-05-06 10:46:08 -0300

    Adding Fugitive keymap for pushing changes with <leader>gp
    It only works inside Fugitive, adds itself and removes itself from
    which-key menu when appropriate using autocmd.

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -3,6 +3,25 @@ return {
 		"tpope/vim-fugitive",
 		config = function()
 			Map("n", "<leader>gs", vim.cmd.Git, "Git Fugitive")
+			local group = vim.api.nvim_create_augroup(
+				"CustomFugitive", { clear = true }
+			)
+			vim.api.nvim_create_autocmd("BufEnter", {
+				pattern = "fugitive://*//",
+				callback = function()
+					Map("n", "<leader>gp", "<cmd>Git push<cr>", "Git Push")
+				end,
+				group = group,
+			})
+			vim.api.nvim_create_autocmd("BufLeave", {
+				pattern = "fugitive://*//",
+				callback = function()
+					local wk = require("which-key")
+					wk.register({ ["<leader>gp"] = "which_key_ignore" })
+					vim.keymap.del("n", "<leader>gp")
+				end,
+				group = group,
+			})
 		end,
 	}
 }
