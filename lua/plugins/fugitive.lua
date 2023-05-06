acb81b58d0abfe14ce1918d78b81eb1f09af3dae
Franco Schmidt <francosr98@gmail.com>
2023-05-06 11:01:59 -0300

    Adding autocmd description and fixing bug on last commit
    After the first time leaving the Fugitive buffer, since which-key now
    had a "which_key_ignore" tag associated with that key combination, it
    stopped appearing on which-key even when it should. The solution
    provided might not be ideal, but it is good enough while which-key
    doesn't add functionality to remove a keybind.

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -7,13 +7,18 @@ return {
 				"CustomFugitive", { clear = true }
 			)
 			vim.api.nvim_create_autocmd("BufEnter", {
+				desc = "Adds <leader>gp as a keymap to \"git push\" inside Fugitive",
 				pattern = "fugitive://*//",
 				callback = function()
+					local wk = require("which-key")
+					wk.register({ ["<leader>gp"] = "Git Push" })
 					Map("n", "<leader>gp", "<cmd>Git push<cr>", "Git Push")
 				end,
 				group = group,
 			})
 			vim.api.nvim_create_autocmd("BufLeave", {
+				desc = "Removes <leader>gp as a keymap to \"git push\" "
+					.. "when leaving Fugitive",
 				pattern = "fugitive://*//",
 				callback = function()
 					local wk = require("which-key")
