e9b5e8890af7fd68894150c202283cb4a06c2289
Franco Schmidt <francosr98@gmail.com>
2023-05-30 09:26:51 -0300

    Adding better folding with nvim-ufo

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -60,5 +60,26 @@ return {
 				delay = 0,
 			})
 		end
-	}
+	},
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = { "kevinhwang91/promise-async" },
+		config = function()
+			local ufo = require("ufo")
+
+			vim.o.foldcolumn = "1"
+			vim.o.foldlevel = 99
+			vim.o.foldlevelstart = 99
+			vim.o.foldenable = true
+
+			Map("n", "zR", ufo.openAllFolds, "Open all folds")
+			Map("n", "zM", ufo.closeAllFolds, "Close all folds")
+
+			ufo.setup({
+				provider_selector = function(bufnr, filetype, buftype)
+					return { "treesitter", "indent" }
+				end
+			})
+		end
+	},
 }
